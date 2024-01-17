eab8fd2f61d564459ba450009015ff119e8b671f
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-17 11:08:24 +0330

    feat(auth): user auth validation and routes (not completed)

---
+++ app/http/validators/user/auth.schema.js
@@ -0,0 +1,12 @@
+const joi = require('@hapi/joi')
+const authSchema = joi.object({
+    mobile: joi
+        .string()
+        .length(11)
+        .pattern(/^09[0-9]{9}$/)
+        .error(new Error('شماره موبایل وارد شده معتبر نیست')),
+})
+
+module.exports = {
+    authSchema,
+}
--- app/router/router.js
+++ app/router/router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const { HomeRoutes } = require('./api/index.router')
+const { AuthRoutes } = require('./user/auth.router')
 const router = Router()
 
 router.use('/', HomeRoutes)
+router.use('/user', AuthRoutes)
 
 module.exports = {
     AllRoutes: router,

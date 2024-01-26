9a393da078cebdd70cd903fa3abdbf93dfd96fc7
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-26 23:20:21 +0330

    fix(blog): set access token in blog page

--- app/router/admin/admin.routes.js
+++ app/router/admin/admin.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
+const { verifyAccessToken } = require('../../http/middlewares/verifyAccessToken.middleware')
+const { AdminBlogRoutes } = require('./blog/blog.routes')
 const { CategoryRoutes } = require('./category/category.routes')
 
 router.use('/category', CategoryRoutes)
+router.use('/blog', verifyAccessToken, AdminBlogRoutes)
 
 module.exports = {
     AdminRoutes: router,

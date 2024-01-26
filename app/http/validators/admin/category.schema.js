d102475cb52d73e02736c40382602d5924adc1a4
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-26 23:18:04 +0330

    fix(validator): use 'createHttpError' module for error

--- app/http/validators/admin/category.schema.js
+++ app/http/validators/admin/category.schema.js
@@ -1,12 +1,18 @@
 const joi = require('@hapi/joi')
 const { MONGO_ID_PATTERN } = require('../../../utils/constants/constants')
+const createHttpError = require('http-errors')
 const addCategorySchema = joi.object({
-    title: joi.string().required().min(3).max(50).error(new Error('عنوان دسته بندی اشتباه است')),
+    title: joi
+        .string()
+        .required()
+        .min(3)
+        .max(50)
+        .error(createHttpError.BadRequest('عنوان دسته بندی اشتباه است')),
     parent: joi
         .string()
         .pattern(MONGO_ID_PATTERN)
         .allow('')
-        .error(new Error('شناسه ارسال شده صحیح نمی باشد')),
+        .error(createHttpError.BadRequest('شناسه ارسال شده صحیح نمی باشد')),
 })
 
 module.exports = {
--- app/http/validators/user/auth.schema.js
+++ app/http/validators/user/auth.schema.js
@@ -1,10 +1,11 @@
 const joi = require('@hapi/joi')
+const createHttpError = require('http-errors')
 const getOtpSchema = joi.object({
     mobile: joi
         .string()
         .length(11)
         .pattern(/^09[0-9]{9}$/)
-        .error(new Error('شماره موبایل وارد شده معتبر نیست')),
+        .error(createHttpError.BadRequest('شماره موبایل وارد شده معتبر نیست')),
 })
 
 const checkOtpSchema = joi.object({
@@ -12,8 +13,12 @@ const checkOtpSchema = joi.object({
         .string()
         .length(11)
         .pattern(/^09[0-9]{9}$/)
-        .error(new Error('شماره موبایل وارد شده معتبر نیست')),
-    code: joi.string().min(4).max(6).error(new Error('کد ارسال شده صحیح نمی باشد')),
+        .error(createHttpError.BadRequest('شماره موبایل وارد شده معتبر نیست')),
+    code: joi
+        .string()
+        .min(4)
+        .max(6)
+        .error(createHttpError.BadRequest('کد ارسال شده صحیح نمی باشد')),
 })
 
 module.exports = {

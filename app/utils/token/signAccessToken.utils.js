a8d2f436c46a50c880c3eec55feab358cf8aa7a5
Ali-HSNZ <Aliatraby@gmail.com>
2024-01-26 23:16:21 +0330

    refactor(auth): 'signAccessToken' and 'signRefreshToken'

--- app/utils/token/signAccessToken.utils.js
+++ app/utils/token/signAccessToken.utils.js
@@ -5,13 +5,11 @@ const { UserModel } = require('../../models/users.model')
 const signAccessToken = (userId) => {
     return new Promise(async (resolve, reject) => {
         const user = await UserModel.findById(userId)
-        const payload = {
-            mobile: user.mobile,
-        }
-        const options = {
-            expiresIn: '1h',
-        }
-        jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET_KEY, options, (err, token) => {
+        const payload = { mobile: user.mobile }
+        const secretKey = process.env.ACCESS_TOKEN_SECRET_KEY
+        const options = { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+
+        jwt.sign(payload, secretKey, options, (err, token) => {
             if (err) {
                 throw reject(createHttpError.InternalServerError('خطای سمت سرور :)'))
             }
--- app/utils/token/signRefreshToken.utils.js
+++ app/utils/token/signRefreshToken.utils.js
@@ -7,14 +7,11 @@ const signRefreshToken = (userId) => {
     return new Promise(async (resolve, reject) => {
         const user = await UserModel.findById(userId)
 
-        const payload = {
-            mobile: user.mobile,
-        }
+        const payload = { mobile: user.mobile }
+        const secretKey = process.env.REFRESH_TOKEN_SECRET_KEY
+        const options = { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
 
-        const options = {
-            expiresIn: '1y',
-        }
-        jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET_KEY, options, async (err, token) => {
+        jwt.sign(payload, secretKey, options, async (err, token) => {
             if (err) {
                 return reject(createHttpError.InternalServerError('خطای سمت سرور :)'))
             }

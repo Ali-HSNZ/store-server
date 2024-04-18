4395764b4bd65335925c159ef3ee546da20d4f81
Ali-HSNZ <Aliatraby@gmail.com>
2024-04-18 11:10:52 +0330

    feat: config swagger and create swagger ui for home page

--- app/router/api/index.js
+++ app/router/api/index.js
@@ -2,7 +2,7 @@ const homeController = require('../../http/controllers/api/home.controller')
 
 const router = require('express').Router()
 
-router.post('/', homeController.indexPage)
+router.get('/', homeController.indexPage)
 
 module.exports = {
     HomeRoutes: router,
---
+++ app/router/swagger/user/auth/auth.swagger.js
@@ -0,0 +1,15 @@
+/**
+ * @swagger
+ * tags:
+ *      name: Home
+ * /:
+ *     get:
+ *      tags: [Home]
+ *      summary: home page
+ *      description: get base api
+ *      responses:
+ *          200:
+ *              description: success
+ *          400:
+ *              description: not found
+ */

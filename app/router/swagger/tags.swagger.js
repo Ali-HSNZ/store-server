4b457a838f634268f1e1a2e8d8f9911d054a641e
Ali-HSNZ <Aliatraby@gmail.com>
2024-06-27 22:30:40 +0330

    feat: add some tags for swagger

---
+++ app/router/swagger/tags.swagger.js
@@ -0,0 +1,10 @@
+/**
+ * @swagger
+ *  tags:
+ *      -   name: Admin-Panel
+ *          description: Actions of admin (add, remove, edit, and any do)
+ *      -   name: Category (Admin-Panel)
+ *          description: All method about categories
+ *      -   name: Auth
+ *          description: User authentication
+ */

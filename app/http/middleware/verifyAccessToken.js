76ece9726c5cc32334611621977b63fbd1dd58e1
Ali-HSNZ <Aliatraby@gmail.com>
2024-06-30 22:08:32 +0330

    feat: create 'checkRole' middleware

--- app/http/middleware/verifyAccessToken.js
+++ app/http/middleware/verifyAccessToken.js
@@ -2,32 +2,56 @@ const createHttpError = require('http-errors')
 const { UserModel } = require('../../models/users')
 const jwt = require('jsonwebtoken')
 
-const verifyAccessToken = (req, res, next) => {
-    // get token from request header
-    const [bearer, token] = req.headers?.['access-token']?.split(' ') || []
+// get token from request header
+const getToken = (headers) => {
+    const [bearer, token] = headers?.['access-token']?.split(' ') || []
 
-    if (token && bearer.toLowerCase() === 'bearer') {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, async (err, payload) => {
-            // check initial error
-            if (err) return next(createHttpError.Unauthorized('مجددا وارد حساب کاربری خود شوید'))
-
-            // find user from DB with mobile
-            const user = await UserModel.findOne(
-                { mobile: payload.mobile },
-                { mobile: 1, roles: 1 }
-            )
-
-            //  if not Exist User throw error
-            if (!user) return next(createHttpError.Unauthorized('مجددا وارد حساب کاربری خود شوید'))
-
-            // save user info to req
-            req.user = user
-        })
-
-        // everything is ok
-        return next()
-    }
-    return next(createHttpError.Unauthorized('مجددا وارد حساب کاربری خود شوید'))
+    if (token && bearer.toLowerCase() === 'bearer') return token
+    throw createHttpError.Unauthorized('حساب کاربری شناسایی نشد. وارد حساب کاربری خود شوید')
 }
 
-module.exports = { verifyAccessToken }
+const verifyAccessToken = (req, res, next) => {
+    try {
+        const token = getToken(req.headers)
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, async (err, payload) => {
+            try {
+                // check initial error
+                if (err) throw createHttpError.Unauthorized('مجددا وارد حساب کاربری خود شوید')
+
+                // find user from DB with mobile
+                const user = await UserModel.findOne(
+                    { mobile: payload.mobile },
+                    { mobile: 1, roles: 1 }
+                )
+
+                //  if not Exist User throw error
+                if (!user) throw createHttpError.Unauthorized('مجددا وارد حساب کاربری خود شوید')
+
+                // save user info to req
+                req.user = user
+
+                // everything is ok
+                return next()
+            } catch (error) {
+                next(error)
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const checkRole = (role) => {
+    return (req, res, next) => {
+        try {
+            const user = req.user
+            if (user.roles.includes(role)) return next()
+            throw createHttpError.Forbidden('شما به این قسمت دسترسی ندارید')
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = { verifyAccessToken, checkRole }
--- app/router/routes/router.js
+++ app/router/routes/router.js
@@ -2,13 +2,13 @@ const { BlogRoutes } = require('./admin/blog.routes')
 const { CategoryRoutes } = require('./admin/category.routes')
 const { HomeRoutes } = require('./api/home.routes')
 const { AuthRoutes } = require('./user/auth.routes')
-const { verifyAccessToken } = require('../../http/middleware/verifyAccessToken')
+const { verifyAccessToken, checkRole } = require('../../http/middleware/verifyAccessToken')
 const router = require('express').Router()
 
 router.use('/auth', AuthRoutes)
-router.use('/admin/category', CategoryRoutes)
-router.use('/admin/blogs', verifyAccessToken, BlogRoutes)
-router.use('/', HomeRoutes)
+router.use('/admin/category', verifyAccessToken, checkRole('ADMIN'), CategoryRoutes)
+router.use('/admin/blogs', verifyAccessToken, checkRole('ADMIN'), BlogRoutes)
+// router.use('/', HomeRoutes)
 
 module.exports = {
     AllRoutes: router,

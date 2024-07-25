ddd4fd0a1487af29bf7df7b7ab4e2bfb89cb4cee
Ali-HSNZ <Aliatraby@gmail.com>
2024-07-26 00:41:25 +0330

    feat: set permission for routes

--- app/router/routes/router.js
+++ app/router/routes/router.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const { BlogRoutes } = require('./admin/blog.routes')
 const { CategoryRoutes } = require('./admin/category.routes')
 const { AuthRoutes } = require('./user/auth.routes')
-const { verifyAccessToken, checkRole } = require('../../http/middleware/verifyAccessToken')
+const { verifyAccessToken } = require('../../http/middleware/verifyAccessToken')
 const { ProductRoutes } = require('./admin/product.routes')
 const { CourseRoutes } = require('./admin/course.routes')
 const { ChapterRoutes } = require('./admin/chapter.routes')
@@ -11,20 +11,52 @@ const { EpisodeRoutes } = require('./admin/episode.routes')
 const { UserRoutes } = require('./admin/user.routes')
 const { PermissionsRoutes } = require('./admin/permission.routes')
 const { RoleRoutes } = require('./admin/role.routes')
+const { checkPermissions } = require('../../http/middleware/permission.guard')
+const { PERMISSIONS } = require('../../constants')
 
 // PUBLIC ROUTES
 router.use('/auth', AuthRoutes)
 
 // ADMIN ROUTES
-router.use('/admin/category', verifyAccessToken, checkRole('ADMIN'), CategoryRoutes)
-router.use('/admin/blogs', verifyAccessToken, checkRole('ADMIN'), BlogRoutes)
-router.use('/admin/products', verifyAccessToken, checkRole('ADMIN'), ProductRoutes)
-router.use('/admin/courses', verifyAccessToken, checkRole('ADMIN'), CourseRoutes)
-router.use('/admin/chapters', verifyAccessToken, checkRole('ADMIN'), ChapterRoutes)
-router.use('/admin/episodes', verifyAccessToken, checkRole('ADMIN'), EpisodeRoutes)
-router.use('/admin/users', verifyAccessToken, checkRole('ADMIN'), UserRoutes)
-router.use('/admin/permission', verifyAccessToken, checkRole('ADMIN'), PermissionsRoutes)
-router.use('/admin/role', verifyAccessToken, checkRole('ADMIN'), RoleRoutes)
+router.use(
+    '/admin/category',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.ADMIN]),
+    CategoryRoutes
+)
+router.use('/admin/blogs', verifyAccessToken, checkPermissions([PERMISSIONS.TEACHER]), BlogRoutes)
+router.use(
+    '/admin/products',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.SUPPLIER, PERMISSIONS.CONTENT_MANAGER]),
+    ProductRoutes
+)
+router.use(
+    '/admin/courses',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.TEACHER]),
+    CourseRoutes
+)
+router.use(
+    '/admin/chapters',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.TEACHER]),
+    ChapterRoutes
+)
+router.use(
+    '/admin/episodes',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.TEACHER]),
+    EpisodeRoutes
+)
+router.use('/admin/users', verifyAccessToken, UserRoutes)
+router.use(
+    '/admin/permission',
+    verifyAccessToken,
+    checkPermissions([PERMISSIONS.ADMIN]),
+    PermissionsRoutes
+)
+router.use('/admin/role', verifyAccessToken, checkPermissions([PERMISSIONS.ADMIN]), RoleRoutes)
 
 module.exports = {
     AllRoutes: router,

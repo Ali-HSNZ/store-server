a5d01499d1433a40cf44761d948ec1cec1eaf291
Ali-HSNZ <Aliatraby@gmail.com>
2024-07-25 18:53:40 +0330

    feat: permission (get, add, edit, delete) route

--- app/http/controllers/admin/RBAC/permission.controller.js
+++ app/http/controllers/admin/RBAC/permission.controller.js
@@ -1,6 +1,106 @@
 const { Controller } = require('../../controller')
+const { PermissionsModel } = require('../../../../models/permissions')
+const { StatusCodes } = require('http-status-codes')
+const { addPermissionSchema } = require('../../../validators/admin/RBAC.validation')
+const createHttpError = require('http-errors')
+const { copyObject } = require('../../../../utils')
 
-class PermissionController extends Controller {}
+class PermissionController extends Controller {
+    async getAll(req, res, next) {
+        try {
+            const permissions = await PermissionsModel.find({})
+            return res.status(StatusCodes.OK).json({
+                statusCode: StatusCodes.OK,
+                data: {
+                    permissions,
+                },
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async add(req, res, next) {
+        try {
+            const { title, description } = await addPermissionSchema.validateAsync(req.body)
+
+            await this.findByTitle(title)
+
+            const permission = await PermissionsModel.create({ title, description })
+
+            if (!permission) throw createHttpError.InternalServerError('خطای سرور')
+
+            return res.status(StatusCodes.CREATED).json({
+                statusCode: StatusCodes.CREATED,
+                data: {
+                    message: 'سطح دسترسی با موفقیت افزوده شد',
+                },
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async edit(req, res, next) {
+        try {
+            const { id } = req.params
+
+            await this.findById(id)
+
+            const data = copyObject(req.body)
+
+            deleteInvalidPropertyInObject(data, [])
+
+            const updatePermissionResult = await PermissionsModel.updateOne(
+                { _id: id },
+                { $set: data }
+            )
+
+            if (updatePermissionResult.modifiedCount === 0)
+                throw createHttpError.InternalServerError('خطای سرور')
+
+            return res.status(StatusCodes.OK).json({
+                statusCode: StatusCodes.OK,
+                data: {
+                    message: 'به روزرسانی سطح دسترسی با موفقیت انجام شد',
+                },
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(req, res, next) {
+        try {
+            const { id } = req.params
+            await this.findById(id)
+
+            const removePermissionResult = await PermissionsModel.deleteOne({ _id: id })
+
+            if (removePermissionResult.deletedCount === 0)
+                throw createHttpError.InternalServerError('خطای سرور')
+
+            return res.status(StatusCodes.OK).json({
+                statusCode: StatusCodes.OK,
+                data: {
+                    message: 'سطح دسترسی با موفقیت حذف شد',
+                },
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async findByTitle(title) {
+        const permission = await PermissionsModel.findOne({ title })
+        if (permission) throw createHttpError.BadRequest('دسترسی قبلا ثبت شده است')
+    }
+    async findById(_id) {
+        const permission = await PermissionsModel.findOne({ _id })
+        if (!permission) throw createHttpError.NotFound('دسترسی یافت شده است')
+        return permission
+    }
+}
 
 module.exports = {
     PermissionController: new PermissionController(),
--- app/router/routes/admin/permission.routes.js
+++ app/router/routes/admin/permission.routes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router()
 
+const {
+    PermissionController,
+} = require('../../../http/controllers/admin/RBAC/permission.controller')
+
+// GET
+router.get('/get-all', PermissionController.getAll)
+
+// POST
+router.post('/add', PermissionController.add)
+
+// PATCH
+router.post('/edit-by-id', PermissionController.edit)
+
+// DELETE
+router.delete('/remove/:id', PermissionController.remove)
+
 module.exports = { PermissionsRoutes: router }

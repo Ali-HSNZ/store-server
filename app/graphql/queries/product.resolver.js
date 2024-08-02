154839643152460e8ff57707661a0b1e5a4316cb
Ali-HSNZ <Aliatraby@gmail.com>
2024-08-02 17:25:57 +0330

    feat: get blogs, product and course with category  id search filter

--- app/graphql/queries/product.resolver.js
+++ app/graphql/queries/product.resolver.js
@@ -1,11 +1,19 @@
-const { GraphQLList } = require('graphql')
+const { GraphQLList, GraphQLString } = require('graphql')
 const { ProductModel } = require('../../models/products')
 const { ProductType } = require('../typeDefs/product.type')
 
 const ProductResolver = {
     type: new GraphQLList(ProductType),
-    resolve: async () => {
-        return await ProductModel.find({}).populate([{ path: 'category' }, { path: 'supplier' }])
+    args: {
+        category: { type: GraphQLString },
+    },
+    resolve: async (_, args) => {
+        const { category } = args
+        const findQuery = category ? { category } : {}
+        return await ProductModel.find(findQuery).populate([
+            { path: 'category' },
+            { path: 'supplier' },
+        ])
     },
 }
 
